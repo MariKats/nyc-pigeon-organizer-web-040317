23e4edea94338552d841e2234fba91d09abe99ce
Marianna Katsika <marianna.katsika@gmail.com>
2017-03-20 18:38:46 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = { }
+  data.each do |attributes, data_hash|
+    data_hash.each do |attribute, array|
+      array.each do |value|
+       if new_hash[value].nil?
+          new_hash[value] = { }
+        end
+        
+        new_hash.each do |value, info|
+            info[:color] = []
+            info[:gender] = []
+            info[:lives] = []
+              data.each do |attributes, data_hash|
+              data_hash.each do |attribute, array|
+              if data[attributes][attribute].include?(value)
+                new_hash[value][attributes] << attribute.to_s
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+  new_hash
 end
